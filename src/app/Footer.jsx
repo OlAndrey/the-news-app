d27e32105147f6243dbcb233516fde7c0a952f0f
OlAndrey <oleynik.andrey01@gmail.com>
2023-04-30 23:50:08 +0300

    feat: created and styled footer

---
+++ src/app/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="flex-initial max-w-6xl py-5 mx-auto border-t text-center">
+        Make by{' '}
+        <a
+          href="https://github.com/OlAndrey"
+          className="font-medium text-600 dark:text-500 hover:underline"
+          target="_blank"
+        >
+          Oleynik Andrey
+        </a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,18 +2,20 @@ import React from 'react'
 import '../styles/globals.css'
 import Header from './Header'
 import Head from './head'
+import Footer from './Footer'
 
 export default function RootLayout({ children }) {
   return (
     <html>
-        <Head />
-        <body
-          className="bg-gray-100
+      <Head />
+      <body
+        className="flex flex-col bg-gray-100
        dark:bg-zinc-900 transition-all-duration-700"
-        >
-          <Header />
-          <div className="max-w-6xl mx-auto">{children}</div>
-        </body>
+      >
+        <Header />
+        <div className="flex-auto flex-shrink-0 max-w-6xl mx-auto">{children}</div>
+        <Footer />
+      </body>
     </html>
   )
 }
