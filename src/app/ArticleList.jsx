f7e8ce91ed2fa656acbeacce1957afb149720393
OlAndrey <oleynik.andrey01@gmail.com>
2023-05-02 00:12:16 +0300

    chore(ArticleList): the height of the title is limited and the data about the article is moved to the function

--- src/app/ArticleList.jsx
+++ src/app/ArticleList.jsx
@@ -2,68 +2,73 @@ import React from 'react'
 import Image from 'next/image'
 
 const ArticleList = ({ articlesDate }) => {
+  const ArticleTitle = ({ date, title }) => {
+    return (
+      <>
+        <p className="tex-red-500">{new Date(date).toLocaleDateString()}</p>
+        <h3 className="text-2xl tex-red-700 text-ellipsis line-clamp-3">
+          {title}
+        </h3>
+      </>
+    )
+  }
+
+  const ArticleImage = ({ src }) => {
+    return (
+      <Image
+        className="top-0 left-0 object-cover"
+        src={src}
+        alt="Picture of the author"
+        width={100}
+        height={60}
+        layout="responsive"
+        objectFit="cover"
+        quality={90}
+      />
+    )
+  }
+
   return (
     <div className="flex flex-wrap mx-auto">
       <div className="w-full p-2 rounded md:w-1/2 relative">
-        <Image
-          className="top-0 left-0 object-cover"
-          src={articlesDate[0].image}
-          alt="Picture of the author"
-          width={100}
-          height={60}
-          layout="responsive"
-          objectFit="cover"
-          quality={90}
-        />
+        <ArticleImage src={articlesDate[0].image} />
         <div className="bg-black/30 absolute top-0 left-0" />
         <div className="absolute text-white bottom-0.5 left-1/4 -translate-x-1/4 -translate-y-1/4">
-          <p className="tex-red-500">
-            {new Date(articlesDate[0].published_at).toLocaleDateString()}
-          </p>
-          <h3 className="text-2xl tex-red-700">{articlesDate[0].title}</h3>
+          <ArticleTitle
+            date={articlesDate[0].published_at}
+            title={articlesDate[0].title}
+          />
         </div>
       </div>
 
-      <div className="flex flex-col w-full p-2 rounded md:w-1/2">
-        {articlesDate.slice(1, 3).map((value) => {
+      <div className="flex flex-col w-full rounded md:w-1/2">
+        {articlesDate.slice(1, 3).map((articleDate) => {
           return (
             <div
-              key={value.id}
+              key={articleDate.id}
               className={'w-full p-2 flex flex-wrap rounded '}
             >
               <div className="pr-2 md:w-1/2">
-                <Image
-                  src={value.image}
-                  alt="Picture of the author"
-                  width={100}
-                  height={60}
-                  layout="responsive"
-                  objectFit="cover"
-                  quality={90}
-                />
+                <ArticleImage src={articleDate.image} />
               </div>
               <div className="md:w-1/2">
-                <p>{new Date(value.published_at).toLocaleDateString()}</p>
-                <h3>{value.title}</h3>
+                <ArticleTitle
+                  date={articleDate.published_at}
+                  title={articleDate.title}
+                />
               </div>
             </div>
           )
         })}
       </div>
-      {articlesDate.slice(3, 6).map((value) => {
+      {articlesDate.slice(3, 6).map((articleDate) => {
         return (
-          <div key={value.id} className={'w-full p-2 rounded md:w-1/3'}>
-            <Image
-              src={value.image}
-              alt="Picture of the author"
-              width={100}
-              height={60}
-              layout="responsive"
-              objectFit="cover"
-              quality={90}
+          <div key={articleDate.id} className={'w-full p-2 rounded md:w-1/3'}>
+            <ArticleImage src={articleDate.image} />
+            <ArticleTitle
+              date={articleDate.published_at}
+              title={articleDate.title}
             />
-            <p>{new Date(value.published_at).toLocaleDateString()}</p>
-            <h3>{value.title}</h3>
           </div>
         )
       })}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ const data = new Array(6).fill(0).map((val, i) => {
   return {
     id: i + Math.floor(Math.random() * 100000),
     author: 'Michael Rothstein',
-    title: 'New Lions safety Jayron Kearse suspended three games',
+    title: 'New Lions safety Jayron Kearse suspended three games. New Lions safety Jayron Kearse suspended three games. New Lions safety Jayron Kearse suspended three games',
     description:
       'Kearse, 26, signed with the Lions in March after four seasons in Minnesota, where he played in 62 games with five starts, making 79 tackles and defending eight passes.',
     url: 'https://www.espn.com/nfl/story/_/id/29572640/new-lions-safety-jayron-kearse-suspended-three-games',
