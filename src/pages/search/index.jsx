c0595f8ed5b7751b2ffc65f06d70313d9435df86
OlAndrey <oleynik.andrey01@gmail.com>
2023-05-07 23:31:44 +0300

    feat(searchPage): added and styled search result page

---
+++ src/pages/search/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import axios from 'axios'
+import NewsList from '@/components/NewsList'
+
+export async function getServerSideProps(context) {
+  const { term } = context.query
+  try {
+    const res = await axios.get(`${process.env.FETCH_URL}search=${term}&limit=24`)
+    const resData = await res.data?.data
+
+    return { props: { dataNews: resData, term } }
+  } catch (error) {
+    console.log(error)
+    return { props: { dataNews: [], term } }
+  }
+}
+
+const SearchPage = ({ dataNews, term }) => {
+  if (!dataNews.length)
+    return <div>Keyword search {term} did not return any results.</div>
+
+  return (
+    <div>
+      <h1 className="headerTitle">Search Results for: {term}</h1>
+      <NewsList articlesDate={dataNews} />
+    </div>
+  )
+}
+
+export default SearchPage
